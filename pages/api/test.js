2f8bce1a8d794deb9eab10064abb4c6dbec79eb5
Todd Gardner <todd@toddhgardner.com>
2020-07-17 10:51:09 -0500

    install?

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,5 +1,12 @@
 import { TrackJS } from "../../utils/trackjs-isomorphic.js";
 
+if (!TrackJS.isInstalled()) {
+  TrackJS.install({
+    token: "fake",
+    usageURL: "https://nextjs-tau-azure.vercel.app/api/timeout"
+  });
+}
+
 export default (req, res) => {
 
   TrackJS.usage();
