122e16d37c395ea4b58b22d9bd51366dc934d9a4
Todd Gardner <todd@toddhgardner.com>
2020-07-17 19:56:00 -0500

    without trackjs

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,15 +1,15 @@
 import { TrackJS } from "../../utils/trackjs-isomorphic.js";
 
 if (!TrackJS.isInstalled()) {
-  TrackJS.install({
-    token: "fake",
-    usageURL: "https://nextjs-tau-azure.vercel.app/api/timeout"
-  });
+  // TrackJS.install({
+  //   token: "fake",
+  //   usageURL: "https://nextjs-tau-azure.vercel.app/api/timeout"
+  // });
 }
 
 export default (req, res) => {
 
-  TrackJS.usage();
+  // TrackJS.usage();
   res.status(200).json({ text: "fuckyou" });
 
 }
